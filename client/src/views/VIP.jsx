b4c08b8bc91e55bddba5db0271e3c5b51307c93e
Alexander Rowland <arowland900@gmail.com>
2018-07-23 15:04:50 -0700

    edits to VIP page, starting to build out synth

--- client/src/views/VIP.jsx
+++ client/src/views/VIP.jsx
@@ -2,9 +2,35 @@ import React from 'react';
 
 const VIP = (props) => {
     return (
-        <div className="VIP">
-            <h1>Your Synths Go here!</h1>
+    <div>
+        <h3>WaveForm:</h3>
+        <select id="waveform-select">
+        <option value="sine">Sine</option>
+        <option value="triangle">Triangle</option>
+        <option value="sawtooth">Sawtooth</option>
+        <option selected value="square">Square</option>
+        </select>
+
+        <div className="envelope">
+        <h3>Envelope:</h3>
+        {/* Attack:
+        <input id="attack" type="range" value="20">
+        Release:
+        <input id="release" type="range" value="20"> */}
         </div>
+
+        <div id="keyboard">
+        <button class="key" data-freq="200">SOUND</button>
+        <button class="key" data-freq="250">SOUND</button>
+        <button class="key" data-freq="300">SOUND</button>
+        <button class="key" data-freq="350">SOUND</button>
+        <button class="key" data-freq="400">SOUND</button>
+        </div>
+
+        <script src="js/app.js" charset="utf-8"></script>
+
+    </div>
+
     )
 }
 
